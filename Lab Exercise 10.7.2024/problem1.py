d2050b3314b5dc26991a6ae1b25bb252e10f17a1
nmessa <nmessa@sau16.org>
2024-10-04 14:18:11 -0400

    Add files via upload

---
+++ Lab Exercise 10.7.2024/problem1.py
@@ -0,0 +1,23 @@
+## Lab Exercise 10/7/2024 Problem 1
+## Author: 
+## This program demonstrates a complex function call
+import math
+
+def f(x):
+    #Add code here
+    
+
+def g(x):
+    #Add code here
+    
+
+def h(x):
+    #Add code here
+    
+
+def k(x):
+    #Add code here
+    
+
+#Test code
+print (f(2)) #39.0
