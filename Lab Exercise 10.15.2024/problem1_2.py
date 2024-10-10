9f4eebe5a572a4b6dca70a7ec646658ce77768c5
nmessa <nmessa@sau16.org>
2024-10-10 16:33:50 -0400

    Add files via upload

---
+++ Lab Exercise 10.15.2024/problem1_2.py
@@ -0,0 +1,53 @@
+## Lab Exercise 10/15/2024 Problem 1 and 2
+## Author: 
+## This program will write a name and address to a file
+
+#Get input from user (name, address, city, state, and zipcode
+#Add code here
+
+
+#Create a string that combines city, state and zipcode and store in csz
+#Add code here
+
+
+#write user input to a file
+#open file
+#Add code here
+
+#write information to file
+#Add code here
+
+
+#close the file
+#Add code here
+
+
+#read from a file and read in text
+#open file for reading
+#Add code here
+
+
+#read in lines from file
+#Add code here
+
+
+#close the file
+#Add code here
+
+
+#display the text
+#Add code here
+
+
+    
+##Sample Output
+##Enter your name: Joe Smith
+##Enter your street address: 12 Main Street
+##Enter your city: Exeter
+##Enter your state: NH
+##Enter your ZIP code: 03833
+##Joe Smith
+##12 Main Street
+##Exeter, NH   03833
+
+
