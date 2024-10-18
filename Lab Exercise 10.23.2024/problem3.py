4843e5eb97de58bb2af61dc451a5759450a0ca73
nmessa <nmessa@sau16.org>
2024-10-18 16:11:55 -0400

    Add files via upload

---
+++ Lab Exercise 10.23.2024/problem3.py
@@ -0,0 +1,22 @@
+## Lab Exercise 10/23/2024 Problem 3
+## Author: 
+## Program demonstrates alternating sign sum
+def alternatingSum(lst):
+    total = 0
+    #Add code here
+
+
+    
+    return total
+   
+#Code to test the function
+myList = [1,4,9,16,9,7,4,9,11]
+print (alternatingSum(myList))
+
+## Output
+## -2
+
+
+
+
+
